2874a40dfecd16787024afcfd3ba263d5476878d
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-07-27 18:24:25 -0500

    set up scroll reveal

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-var sideNav = document.querySelector('.main-nav--side-nav');
-var btns = document.querySelectorAll('.button');
+var sideNav = document.querySelector(".main-nav--side-nav");
+var btns = document.querySelectorAll(".button");
 
 btns.forEach(function(btn) {
-    btn.addEventListener('click', function() {
+  btn.addEventListener("click", function() {
+    if (
+      btn.classList.contains("main-nav__close-nav") ||
+      btn.classList.contains("main-nav__open-nav")
+    ) {
+      sideNav.classList.toggle("toggle-menu");
+    }
+  });
+});
 
-        if(btn.classList.contains('main-nav__close-nav') || btn.classList.contains('main-nav__open-nav')) 
-        {
-            sideNav.classList.toggle('toggle-menu');
-        }
-
-    })
-})
+window.sr = ScrollReveal();
+sr.reveal(".cheeries-img", {
+  duration: 1500,
+  //   delay: 500,
+  origin: "right",
+  distance: "200px",
+  opacity: 0,
+  easing: "ease"
+  //   reset: true
+});
+sr.reveal(".fade-left", {
+  origin: "left",
+  opacity: 0,
+  //   reset: true,
+  easing: "ease",
+  distance: "200px",
+  duration: 1500
+});
+sr.reveal(".fade-down", {
+  origin: "top",
+  opacity: 0,
+  //   reset: true,
+  easing: "ease",
+  distance: "200px",
+  duration: 1500
+});
+sr.reveal(".fade-up", {
+  origin: "bottom",
+  opacity: 0,
+  //   reset: true,
+  easing: "ease",
+  distance: "200px",
+  duration: 1500
+});
+sr.reveal(".fade-right", {
+  origin: "right",
+  opacity: 0,
+  //   reset: true,
+  easing: "ease",
+  distance: "200px",
+  duration: 1500
+});
